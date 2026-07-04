907027ac70b1c4b7929acf9f89b1f26622e2c14c
ayushkumar2601 <kumarayush.professional@gmail.com>
2026-07-04 10:14:00 +0530

    initial commit and setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: true
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                required: false,
+                defaultValue: "EMPLOYEE"
+            },
+            companyId: {
+                type: "string",
+                required: false
+            }
+        }
+    }
+});
